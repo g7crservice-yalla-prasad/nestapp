46c433abdae37f157977e5c6f858239c4821873c
g7crservice-yalla-prasad <yalla.prasad@g7cr.com>
2025-10-27 18:17:13 +0530

    Update projects.service.ts

--- src/modules/projects/projects.service.ts
+++ src/modules/projects/projects.service.ts
@@ -16,6 +16,6 @@ export class ProjectsService {
     constructor(){}
 
     getProjects(){
-        return({projects, var: process.env.TEST_VAR && null})
+        return({projects, var: process.env.TEST_VAR || null})
     }
 }
